96711708c7ac2bd7cb6d7fa1dcea5efb9a65ca55
Jinto Jose <jintoppy@gmail.com>
2021-09-23 12:02:25 +0530

    user model

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUsers(){
+    return this.http.get<User[]>('https://api.github.com/users');
+  }
 }
