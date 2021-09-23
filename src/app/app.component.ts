83c2aa7fa24acc446ac212dae33cfddbe6fd2daa
Jinto Jose <jintoppy@gmail.com>
2021-09-23 12:07:01 +0530

    http.get

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AppService } from './app.service';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,16 @@ import { AppService } from './app.service';
 })
 export class AppComponent {
   title = 'angular-http';
+  users:User[] = [];
 
   constructor(private service: AppService){
 
+  }
+
+  ngOnInit(){
+    this.service.getUsers()
+          .subscribe(users => {
+            this.users = users;
+          });
   }
 }
